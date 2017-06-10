72c3f808b0aaa8a09d4bea5c8ee1507bf2b90999
Collo geek/ <collinslenjo.cl@gmail.com>
2017-06-10 11:40:00 +0300

    xxxxxxxxxx

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	client_id: "getpaid-fahxhpwh",
-	secret_key: "DLfFXIQr4aHBK7Q42DlWQ7W7adgN1gEN",
-	token: "VFml6km0EInbIw5GgjawYgB5Jlw5yP11"
+	client_id: "xxxxxxxxxx",
+	secret_key: "xxxxxxxxxx",
+	token: "xxxxxxxxxx"
 }
